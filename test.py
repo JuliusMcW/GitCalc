0550cbe4409ae9b8b7fa3105ceb31fb981146d9c
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-06 20:39:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,17 +4,17 @@ import unittest
 
 class Tester(unittest.TestCase):
     def test_add(self):
-        result = calc.addition(0, 1)
+        result = example.addition(0, 1)
         self.assertEqual(result, 1)
 
     def test_subtract(self):
-        result = calc.subtraction(4, 2)
+        result = example.subtraction(4, 2)
         self.assertEqual(result, 2)
 
     def test_mul(self):
-        result = calc.multiplication(1, 3)
+        result = example.multiplication(1, 3)
         self.assertEqual(result, 3)
 
     def test_division(self):
-        result = calc.division(20, 5)
+        result = example.division(20, 5)
         self.assertEqual(result, 4)
